144fcaae6bcfe084b4aec9441b43212f6389f02c
Suzanna Pavlovsky <suzanna-imac@Suzannas-iMac.local>
2022-06-04 11:13:35 -0400

    Started working on the frontend UI

---
+++ vulntraq-frontend/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <b-navbar variant="dark">
+        <h1> Hello </h1>
+    </b-navbar>
+    </div>
+</template>
+
+<style lang="scss">
+    .b-navbar {
+        color: #6f42c1;
+    }
+</style>
+
+<script>
+
+export default {
+  name: 'AppHeader',
+  components: {
+  }
+}
+
+</script>
