f6a786baaa158a0653835288cd6569b93aff578b
Suzanna Pavlovsky <suzanna-imac@Suzannas-iMac.local>
2022-06-04 16:35:50 -0400

    Designed the UI's preliminary header

--- vulntraq-frontend/src/components/AppHeader.vue
+++ vulntraq-frontend/src/components/AppHeader.vue
@@ -1,15 +1,52 @@
 <template>
     <div>
-    <b-navbar variant="dark">
-        <h1> Hello </h1>
-    </b-navbar>
+        <b-navbar toggleable="lg" class="header-navbar">
+            <img src="../assets/logo.png" class="logo-img"/>
+            <h1 class="logo-text">
+                VulnTraq
+            </h1>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ms-auto">
+                <b-button v-on:click="openSettings()" class="settings-button" style="background-color:transparent">
+                    <b-icon icon="gear-fill" aria-hidden="true"></b-icon> 
+                </b-button>
+            </b-navbar-nav>
+
+            <div style="width: 20px;"/>
+            
+        </b-navbar>
     </div>
 </template>
-
+    
 <style lang="scss">
-    .b-navbar {
-        color: #6f42c1;
-    }
+
+// Styles the navigation bar
+.header-navbar {
+    height: 60px;
+    background-color: purple;
+}
+
+// Styles the logo image appearing in the navigation bar
+.logo-img {
+    height: 30px;
+    width: 30px;
+    margin-left: 20px;
+    vertical-align: center;
+}
+
+// Styles the "VulnTraq" logo text appearing in the navigation bar
+.logo-text {
+    font-family: "Arial Narrow";
+    padding-left: 20px;
+    color: white;
+}
+
+// Styles the settings button
+.settings-button {
+    margin-right: 20px;
+}
+
 </style>
 
 <script>
@@ -17,6 +54,11 @@
 export default {
   name: 'AppHeader',
   components: {
+  },
+  methods: {
+      openSettings: function openSettings() {
+          console.log("DEBUG: in open settings function");
+      }
   }
 }
 
